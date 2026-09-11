4bddfa747c1e57f66fa22f6b4ad87cd9987e0116
Dea Widya Ningsih <2581009@unai.edu>
2026-09-11 10:45:52 +0700

    menambahkan harga barang

---
+++ src/StrukBelanja.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class StrukBelanja {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int hargaBuku = 5000;
+        int hargaPulpen = 3000;
+        int hargaPenghapus = 2000;
+
+
+    }
+}
